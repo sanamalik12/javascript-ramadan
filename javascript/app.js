21b15ac54e4a0342f5a0af2a67b34216b44ae9c6
sanamalik12 <129421999+sanamalik12@users.noreply.github.com>
2023-03-31 00:40:20 -0700

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,62 @@
+alert("Thank you for your input")
+console.log("welcome")
+var name = "mark";
+
+var orignalnum = 23;
+var newnum = orignalnum + 7;
+console.log(newnum)
+
+var orignalnum = 23;
+var numtobeadded = - 7;
+var newnum = orignalnum + numtobeadded;
+console.log(newnum)
+
+
+var orignalnum ="23";
+var newnumber = + 7;
+var newnam = orignalnum + newnumber;
+
+console.log(newnam)
+
+var poularnumber =4*4
+console.log(poularnumber) 
+
+var poularnumber =4-4
+console.log(poularnumber) 
+
+var poularnumber =4+4
+console.log(poularnumber)
+
+var num = 10;
+var antotherm =1;
+var popularnumber = num + antotherm;
+console.log(popularnumber)
+
+var resulttocomputation =(2 + 4) * 4 + 2;
+console.log(resulttocomputation)
+
+var message = "THANKS,";
+var username = "sana";
+var banger = "!"
+var custom = - message + username + banger;
+console.log(custom)
+
+var firstname = prompt("entername");
+console.log(firstname);
+
+var lastname =prompt("lastname");
+console.log(firstname + "" + lastname )
+
+var city = prompt("entercity", "karachi");
+console.log(firstname +" "+ lastname + " from " + city);
+
+var x = prompt("Where does the people live?");
+if (x === "vatican"){ alert("correct!");}
+console.log(x)
+
+// if (yourticketnumber !==  48702){ alert("better luck next time.");
+// console.log(yourticketnumber)}
+var thirtyrozahonga =true;
+if(thirtyrozahongyatwentynine = true) {console.log("maybethirty");
+}
+else{console.log("maybetwentynine");}
